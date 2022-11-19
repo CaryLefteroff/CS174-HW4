80d8fc5686a7dcff697120ea80a6e62a9ba53dae
Cary Lefteroff <cary.lefteroff@sjsu.edu>
2022-11-18 18:50:23 -0800

    JS quiz validation

--- views/QuizPageContent.php
+++ views/QuizPageContent.php
@@ -18,7 +18,26 @@ class QuizPageContent extends View {
     $questions = $controller -> generateQuestions("language");
     ?>
     <div id="quiz-page">
-        <form name="quiz-selections" method="post" action="index.php">
+        <script type="text/javascript">
+            function checkBoxes() {
+                for(var i = 1; i <= 20; i++) {
+                    let name = "answer" + i + "_"
+                    let a = "#" + name + 1 + ":checked";
+                    let b = "#" + name + 2 + ":checked";
+                    let c = "#" + name + 3 + ":checked";
+                    let d = "#" + name + 4 + ":checked";
+                    let one = document.querySelector(a) !== null;
+                    let two = document.querySelector(b) !== null;
+                    let three = document.querySelector(c) !== null;
+                    let four = document.querySelector(d) !== null;
+                    if(!(one || two || three || four)) { //If none of the checkboxes are checked...
+                        return false;
+                    }
+                }
+                return true;
+            }
+        </script>
+        <form name="quiz-selections" method="post" action="index.php" onsubmit="return checkBoxes()">
             <div>
             <p>Select the words that could be used to fill in the blank (at least one should work).</p>
             </div>
@@ -52,13 +71,13 @@ class QuizPageContent extends View {
             <h5><?php echo $prompt; ?></h5>
             <span>
                 <input type="hidden" name=<?= $word ?> value=<?= $percentile ?>>
-                <input type="checkbox" name="answer<?$index?>_1"/>
+                <input type="checkbox" id="answer<?$index?>_1" name="answer<?$index?>_1"/>
                 <label for="answer<?$index?>_1"><?php echo $a1;?></label>
-                <input type="checkbox" name="answer<?$index?>_2"/>
+                <input type="checkbox" id="answer<?$index?>_1" name="answer<?$index?>_2"/>
                 <label for="answer<?$index?>_2"><?php echo $a2;?></label>
-                <input type="checkbox" name="answer<?$index?>_3"/>
+                <input type="checkbox" id="answer<?$index?>_1" name="answer<?$index?>_3"/>
                 <label for="answer<?$index?>_3"><?php echo $a3;?></label>
-                <input type="checkbox" name="answer<?$index?>_4"/>
+                <input type="checkbox" id="answer<?$index?>_1" name="answer<?$index?>_4"/>
                 <label for="answer<?$index?>_4"><?php echo $a4;?></label>
             </span>
         </div>
